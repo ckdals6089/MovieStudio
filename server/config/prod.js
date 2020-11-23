dc65e18ccd497e99d1fb1930ca9a7a0333393bc5
Changmin Shin <34693560+ckdals6089@users.noreply.github.com>
2020-11-22 23:17:31 -0500

    Update prod.js

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://dbAddsdsdmin:adsdadasdsmin@moviestudio.rjb19.mongodb.net/MovieStudio?retryWrites=true&w=majority'
+    mongoURI:process.env.MONGO_URI
 }
