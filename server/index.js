d7647ebe3760beb7b604e83bda4f9ee881f3b701
Changmin Shin <ckdals6089@gmail.com>
2020-11-22 23:09:32 -0500

    Fix an error that connect mongodb with backend

--- server/index.js
+++ server/index.js
@@ -4,10 +4,10 @@ const path = require("path");
 const cors = require('cors')
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const config = require("./config/key");
+const config = require("./config/dev");
 const mongoose = require("mongoose");
 
-const connect = mongoose.connect('mongodb+srv://dbAdmin:admin@moviestudio.rjb19.mongodb.net/<dbname>?retryWrites=true&w=majority',
+const connect = mongoose.connect(config.mongoURI,
     {
         useNewUrlParser: true, useUnifiedTopology: true,
         useCreateIndex: true, useFindAndModify: false
@@ -39,7 +39,7 @@ app.use('/uploads', express.static('uploads'));
 //     });
 // }
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 
 app.listen(port, () => {
     console.log(`Server Listening on ${port}`)
