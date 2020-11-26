4f25773e283b43c6d2569884000dbef78ff27a01
Changmin Shin <ckdals6089@gmail.com>
2020-11-26 09:23:31 -0500

    Fix bug on registerpage

--- client/src/components/views/RegisterPage/RegisterPage.js
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -1,9 +1,11 @@
 import React from "react";
 import moment from "moment";
+import { useHistory } from "react-router-dom";
 import { Formik } from 'formik';
 import * as Yup from 'yup';
 import { registerUser } from "../../../_actions/user_actions";
 import { useDispatch } from "react-redux";
+
 import {
   Form,
   Input,
@@ -34,6 +36,7 @@ const tailFormItemLayout = {
 };
 
 function RegisterPage(props) {
+  let history = useHistory();
   const dispatch = useDispatch();
   return (
 
@@ -60,9 +63,9 @@ function RegisterPage(props) {
           .oneOf([Yup.ref('password'), null], 'Passwords must match')
           .required('Confirm Password is required')
       })}
+
       onSubmit={(values, { setSubmitting }) => {
         setTimeout(() => {
-
           let dataToSubmit = {
             email: values.email,
             password: values.password,
@@ -70,15 +73,14 @@ function RegisterPage(props) {
             lastname: values.lastname,
             image: `http://gravatar.com/avatar/${moment().unix()}?d=identicon`
           };
-
           dispatch(registerUser(dataToSubmit)).then(response => {
             if (response.payload.success) {
-              props.history.push("/login");
+              // props.history.push("/login");
+              history.push("/login");
             } else {
               alert(response.payload.err.errmsg)
             }
           })
-
           setSubmitting(false);
         }, 500);
       }}
