6b9bfc44c1bcf7e66b98876690e7b19c856fcb3f
Changmin Shin <ckdals6089@gmail.com>
2020-11-26 19:29:40 -0500

    Create MovieDetail Page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -8,10 +8,12 @@ import Auth from "../hoc/auth";
 import LandingPage from "./views/LandingPage/LandingPage.js";
 import LoginPage from "./views/LoginPage/LoginPage.js";
 import RegisterPage from "./views/RegisterPage/RegisterPage.js";
+import MovieDetail from "./views/MovieDetail/MovieDetail";
 import NavBar from "./views/NavBar/NavBar";
 import Footer from "./views/Footer/Footer"
 
-function App() {
+
+function App(props) {
   return (
     <Router>
       <Suspense fallback={(<div>Loading...</div>)}>
@@ -21,6 +23,7 @@ function App() {
             <Route exact path="/" component={Auth(LandingPage, null)} />
             <Route exact path="/login" component={Auth(LoginPage, false)} />
             <Route exact path="/register" component={Auth(RegisterPage, false)} />
+            <Route exact path="/movie/:movieId" render={(props) => <MovieDetail {...props} />} />
           </Switch>
         </div>
         <Footer />
