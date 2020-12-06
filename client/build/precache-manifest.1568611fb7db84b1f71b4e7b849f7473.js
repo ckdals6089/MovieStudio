5467978eca226cb6c9bc67f8b29f972ec4d64243
Changmin Shin <ckdals6089@gmail.com>
2020-12-05 20:02:04 -0500

    add build

---
+++ client/build/precache-manifest.1568611fb7db84b1f71b4e7b849f7473.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3ec607d694fb37c102251c2693e64df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f19ab6d5ebb619e73313",
+    "url": "/static/css/2.b6e3a137.chunk.css"
+  },
+  {
+    "revision": "2cb210ee51666e4262fc",
+    "url": "/static/css/main.bc39f94d.chunk.css"
+  },
+  {
+    "revision": "f19ab6d5ebb619e73313",
+    "url": "/static/js/2.f457ffc1.chunk.js"
+  },
+  {
+    "revision": "c4a592068352b63b56b164cbe61e876b",
+    "url": "/static/js/2.f457ffc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cb210ee51666e4262fc",
+    "url": "/static/js/main.804986e2.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
